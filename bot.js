cbd3bbd513d8144c767bde71999b8d76a2e80756
Chad Stark <chad.d.stark@hotmail.com>
2021-06-09 19:14:21 -0700

    pushing for laptop

--- bot.js
+++ bot.js
@@ -6,4 +6,26 @@ client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on("voiceStateUpdate", (oldMember, newMember) => {
+  let newUserChannel = newMember.channelID;
+  let oldUserChannel = oldMember.channelID;
+  console.log(1, newUserChannel, 2, oldUserChannel);
+  const textChannel = guild.channels.cache.find(
+    (channel) => channel.name === "general"
+  );
+  console.log(textChannel);
+
+  if (oldUserChannel === null && newUserChannel !== null) {
+    console.log("user has joined a voice channel");
+  } else if (newUserChannel === null) {
+    console.log("user has left voice channel");
+  }
+});
+
+client.on("message", (msg) => {
+  if (msg.content === "announce") {
+    msg.reply("hello", { tts: true });
+  }
+});
+
 client.login(process.env.BOT_LOGIN_TOKEN);
